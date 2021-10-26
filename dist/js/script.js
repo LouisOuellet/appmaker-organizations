cba116156c00daece33014be5a42ed311c5f04c1
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-26 15:53:05 -0400

    21.10-dev-112

--- dist/js/script.js
+++ dist/js/script.js
@@ -446,7 +446,7 @@ API.Plugins.organizations = {
 											API.Helper.set(dataset.details,['issues','raw',sub_dataset.output.raw.id],sub_dataset.output.raw);
 											var subHTML = '';
 											subHTML += '<div class="btn-group m-1" data-id="'+sub_dataset.output.dom.id+'">';
-												subHTML += '<button type="button" data-id="'+subDetails.id+'" class="btn btn-xs bg-'+data.details.statuses.raw[issues[subDetails.id]].color+'" data-action="details"><i class="fas fa-gavel mr-1"></i>'+subDetails.id+' - '+subDetails.name+' - '+API.Contents.Language[data.details.statuses.raw[issues[subDetails.id]].name]+'</button>';
+												subHTML += '<button type="button" data-id="'+sub_dataset.output.dom.id+'" class="btn btn-xs bg-'+data.details.statuses.raw[issues[sub_dataset.output.dom.id]].color+'" data-action="details"><i class="fas fa-gavel mr-1"></i>'+sub_dataset.output.dom.id+' - '+sub_dataset.output.dom.name+' - '+API.Contents.Language[data.details.statuses.raw[issues[sub_dataset.output.dom.id]].name]+'</button>';
 												if(API.Auth.validate('custom', 'issues', 4)){
 													subHTML += '<button type="button" class="btn btn-xs btn-danger" data-id="'+sub_dataset.output.dom.id+'" data-action="unlink"><i class="fas fa-unlink"></i></button>';
 												}
