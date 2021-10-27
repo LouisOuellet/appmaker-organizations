602db0afbd95d4842097b3e83ed89699ca7e4588
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-27 13:14:47 -0400

    21.10-dev-170

--- dist/js/script.js
+++ dist/js/script.js
@@ -71,8 +71,21 @@ API.Plugins.organizations = {
 					// GUI
 					// Adding Layout
 					API.GUI.Layouts.details.build(dataset.output,container,{title:"Organization Details",image:"/dist/img/building.png"},function(data,layout){
-						console.log(data);
-						console.log(layout);
+						// Debug
+						if(API.debug){
+							API.GUI.Layouts.details.control(data,layout,function(data,layout,button){
+								console.log(data);
+								console.log(layout);
+							});
+						}
+						// Clear
+						if(API.Auth.validate('custom', 'organizations_clear', 1)){
+							API.GUI.Layouts.details.control(data,layout,{color:"danger",icon:"fas fa-snowplow",text:"Clear"},function(data,layout,button){
+								button.off().click(function(){
+									API.request('organizations','clear',{ data:data.this.raw });
+								});
+							});
+						}
 						// Name
 						var options = {plugin:"organizations",field:"name"}
 						API.GUI.Layouts.details.data(data,layout,options);
@@ -293,14 +306,6 @@ API.Plugins.organizations = {
 							options.field = "created";
 							options.td = '<td><time class="timeago" datetime="'+data.this.raw.created.replace(/ /g, "T")+'" title="'+data.this.raw.created+'">'+data.this.raw.created+'</time></td>';
 							API.GUI.Layouts.details.data(data,layout,options,function(data,layout,tr){ tr.find('time').timeago(); });
-							// Clear
-							if(API.Auth.validate('custom', 'organizations_clear', 1)){
-								API.GUI.Layouts.details.control(data,layout,{color:"danger",icon:"fas fa-snowplow",text:"Clear"},function(data,layout,button){
-									button.off().click(function(){
-										API.request('organizations','clear',{ data:data.this.raw });
-									});
-								});
-							}
 						}
 					});
 				}
