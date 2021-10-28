d1793370c05b0833e56c8c0b8be5b7919d68f0d6
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-28 15:40:05 -0400

    21.10-dev-237

--- dist/js/script.js
+++ dist/js/script.js
@@ -1380,6 +1380,7 @@ API.Plugins.organizations = {
 							contacts.find('[data-id="'+user.raw.id+'"]').remove();
 							API.Plugins.organizations.GUI.contact(user.dom,contacts);
 							// API.Plugins.organizations.GUI.contacts.add(container, dataset, {dom:user.dom,raw:user.raw}, true);
+							API.Plugins.organizations.Events.contacts(data,layout);
 						});
 						break;
 					case"delete":
