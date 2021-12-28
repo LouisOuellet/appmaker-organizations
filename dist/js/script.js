649c4c1e462545b6352e7fb387831cf3c0c394eb
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-28 10:20:20 -0500

    21.12-dev-535

--- dist/js/script.js
+++ dist/js/script.js
@@ -705,6 +705,46 @@ API.Plugins.organizations = {
 			});
 		},
 	},
+	Timeline:{
+		icon:"building",
+		object:function(dataset,layout,options = {},callback = null){
+			if(options instanceof Function){ callback = options; options = {}; }
+			var defaults = {icon: API.Plugins.organizations.Timeline.icon,color: "secondary"};
+			if(API.Helper.isSet(options,['icon'])){ defaults.icon = options.icon; }
+			if(API.Helper.isSet(options,['color'])){ defaults.color = options.color; }
+			if(typeof dataset.id !== 'undefined'){
+				var dateItem = new Date(dataset.created);
+				var dateUS = dateItem.toLocaleDateString('en-US', {day: 'numeric', month: 'short', year: 'numeric'}).replace(/ /g, '-').replace(/,/g, '');
+				API.Builder.Timeline.add.date(layout.timeline,dataset.created);
+				var checkExist = setInterval(function() {
+					if(layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').length > 0){
+						clearInterval(checkExist);
+						var html = '';
+						html += '<div data-plugin="organizations" data-id="'+dataset.id+'" data-name="'+dataset.name+'" data-date="'+dateItem.getTime()+'">';
+							html += '<i class="fas fa-'+defaults.icon+' bg-'+defaults.color+'"></i>';
+							html += '<div class="timeline-item">';
+								html += '<span class="time"><i class="fas fa-clock mr-2"></i><time class="timeago" datetime="'+dataset.created.replace(/ /g, "T")+'">'+dataset.created+'</time></span>';
+								html += '<h3 class="timeline-header border-0">'+dataset.name+' was linked</h3>';
+							html += '</div>';
+						html += '</div>';
+						layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').after(html);
+						var element = layout.timeline.find('[data-plugin="organizations"][data-id="'+dataset.id+'"]');
+						element.find('time').timeago();
+						var items = layout.timeline.children('div').detach().get();
+						items.sort(function(a, b){
+							return new Date($(b).data("date")) - new Date($(a).data("date"));
+						});
+						layout.timeline.append(items);
+						element.find('i').first().addClass('pointer');
+						element.find('i').first().off().click(function(){
+							API.CRUD.read.show({ key:'id',keys:dataset, href:"?p=organizations&v=details&id="+dataset.name, modal:true });
+						});
+						if(callback != null){ callback(element); }
+					}
+				}, 100);
+			}
+		},
+	},
 	GUI:{
 		contact:function(dataset,layout,plugin = 'contacts'){
 			var area = layout.content[plugin].find('div.row').eq(1);
